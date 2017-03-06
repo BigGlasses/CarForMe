ec796c8b6a8b9729464888585de4afbdc7dded8f
Brandon Roberts <brandonnickroberts@gmail.com>
2017-03-06 15:23:17 -0500

    Now parses data.csv as well

--- raw datasets/parser.py
+++ raw datasets/parser.py
@@ -53,7 +53,33 @@ def compareVehicle(v, model, make, fuelType, fuelType1):
 	else:
 		return -1
 
+def searchVehicles(model ,make, fuelType, fuelType1):
+		lo = 0
+		hi = len(allVehicles) - 1
+		cp = 1
+		while (lo <= hi):
+			mid = lo + (hi - lo) / 2
+			cp = compareVehicle(allVehicles[mid], model, make, fuelType, fuelType1)
+			if (cp > 0):
+				hi = mid - 1
+			elif (cp < 0):
+				lo = mid + 1
+			else:
+				break
+		mid = lo + (hi - lo) / 2
+		if cp == 0:
+			allVehicles[mid].addData()
+		else:
+			newV = vehicle(model, make, fuelType, fuelType1)
+			if cp < 0:
+				allVehicles.insert(mid, newV)
+			else:
+				allVehicles.insert(mid + 1, newV)
+
 vehiclesFieldNames = ("arrels08","barrelsA08","charge120","charge240","city08","city08U","cityA08","cityA08U","cityCD","cityE","cityUF","co2","co2A","co2TailpipeAGpm","co2TailpipeGpm","comb08","comb08U","combA08","combA08U","combE","combinedCD","combinedUF","cylinders","displ","drive","engId","eng_dscr","feScore","fuelCost08","fuelCostA08","fuelType","fuelType1","ghgScore","ghgScoreA","highway08","highway08U","highwayA08","highwayA08U","highwayCD","highwayE","highwayUF","hlv","hpv","id","lv2","lv4","make","model","mpgData","phevBlended","pv2","pv4","range","rangeCity","rangeCityA","rangeHwy","rangeHwyA","trany","UCity","UCityA","UHighway","UHighwayA","VClass","year","youSaveSpend","guzzler","trans_dscr","tCharger","sCharger","atvType","fuelType2","rangeA","evMotor","mfrCode","c240Dscr","charge240b","c240bDscr","createdOn","modifiedOn","startStop","phevCity","phevHwy","phevComb"	)
+datacsvFieldNames = ("year","manufacturer","model","description","euro_standard","tax_band","transmission","transmission_type","engine_capacity","fuel_type","urban_metric","extra_urban_metric","combined_metric","urban_imperial","extra_urban_imperial","combined_imperial","noise_level","co2","thc_emissions","co_emissions","nox_emissions","thc_nox_emissions","particulates_emissions","fuel_cost_12000_miles","fuel_cost_6000_miles","standard_12_months","standard_6_months","first_year_12_months","first_year_6_months","date_of_change")
+
+#Vehicles.csv
 with open('vehicles.csv', 'rb') as csvfile:
 	spamreader = csv.DictReader(csvfile,fieldnames= vehiclesFieldNames, delimiter=',', quotechar='"')
 	ignoreFirst = True
@@ -69,28 +95,30 @@ with open('vehicles.csv', 'rb') as csvfile:
 		#		foundMatch = True
 		#		break
 
-		lo = 0
-		hi = len(allVehicles) - 1
-		cp = 1
-		while (lo <= hi):
-			mid = lo + (hi - lo) / 2
-			cp = compareVehicle(allVehicles[mid], row ['model'], row['make'], row['fuelType'], row['fuelType1'])
-			if (cp > 0):
-				hi = mid - 1
-			elif (cp < 0):
-				lo = mid + 1
-			else:
-				break
-		mid = lo + (hi - lo) / 2
-		if cp == 0:
-			allVehicles[mid].addData()
-		else:
-			newV = vehicle(row ['model'], row['make'], row['fuelType'], row['fuelType1'])
-			if cp < 0:
-				allVehicles.insert(mid, newV)
-			else:
-				allVehicles.insert(mid + 1, newV)
+		searchVehicles(row["model"], row["make"], row["fuelType"], row["fuelType1"])
 		print len(allVehicles)
+
+#data.csv
+with open('data.csv', 'rb') as csvfile:
+	spamreader = csv.DictReader(csvfile,fieldnames= datacsvFieldNames, delimiter=',', quotechar='"')
+	ignoreFirst = True
+	for row in spamreader:
+		if ignoreFirst:
+			ignoreFirst = False
+			continue
+		# row['model'], row ['drive'], row['engId'], row['fuelType'], row['fuelType1'], row['model'], row['trany'], row['year']
+		foundMatch = False
+		#for v in allVehicles: # Make this a binary search
+		#	if v.belongs(row ['model'], row['make'], row['fuelType'], row['fuelType1']):
+		#		v.addData()
+		#		foundMatch = True
+		#		break
+		fuelType = row["fuel_type"]
+		if fuelType.lower() == "petrol":
+			fuelType = "gasoline"
+		searchVehicles(row["model"], row["manufacturer"], fuelType, "regular " + fuelType)
+		print len(allVehicles)
+
 
 for v in allVehicles:
 	print v
