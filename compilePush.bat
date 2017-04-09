6ef3fffd41a8f1d0f241394a3be9b84e0f95871e
Brandon Roberts <brandonnickroberts@gmail.com>
2017-04-09 19:52:39 -0400

    Removed compile batch

--- compilePush.bat
+++
@@ -1,4 +0,0 @@
-call ./gradlew build
-call copy "C:\Users\Brandon\Desktop\2nd Year Term 2\2XB3\CarForMe\build\libs\myapp-0.9.0.war" "C:\Users\Brandon\Desktop\2nd Year Term 2\2XB3\bluemix\get-started-tomcat\target\myapp-0.9.0.war"
-call cd "C:\Users\Brandon\Desktop\2nd Year Term 2\2XB3\bluemix\get-started-tomcat"
-call cf push -b https://github.com/cloudfoundry/java-buildpack.git
